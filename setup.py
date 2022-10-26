01d0f6d62cb8f2eb569020e9085c84db6dc98c0f
tslever <46533664+tslever@users.noreply.github.com>
2022-10-26 05:16:00 -0400

    Revert "Allowed installing booklover with modules in root"
    This reverts commit 45ebf1afbbfe4c834c1d3bce899782081a6ece19.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Tom Lever',
     author_email='tsl2b@virginia.edu',
-    py_modules=['BookAlreadyExistsInBookListException', 'booklover', 'booklover_test'],
+    packages=[],
     url='https://github.com/tslever/booklover',
     license='MIT License',
     description='A Python package offering class BookLover',
